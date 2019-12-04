ebb2e38d5bfcad440b1c46be6abc4dc7c9b2e767
UtkarshKr007 <kumar375@morris.umn.edu>
2019-12-03 19:45:24 -0600

    Start refactor some functions to a setup.clj

---
+++ src/propel/setup.clj
@@ -0,0 +1,107 @@
+(ns propel.setup)
+
+;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTION;;;;;;;;;;;;;;;;;
+
+(def default-instructions
+  (list
+   'in1
+   'exec_dup
+   'exec_if
+   'boolean_and
+   'boolean_or
+   'boolean_not
+   'boolean_=
+   'string_=
+   'string_take
+   'string_drop
+   'string_reverse
+   'string_concat
+   'string_length
+   'string_includes?
+   'close
+   0
+   1
+   true
+   false
+   ""
+   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+   "A"
+   "C"
+   "G"
+   "T"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;:SELECTIONS;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn tournament-selection
+  "Selects an individual from the population using a tournament."
+  [pop argmap]
+  (let [tournament-size (:tournament-size argmap)
+        tournament-set (take tournament-size (shuffle pop))]
+    (apply min-key :total-error tournament-set)))
+
+(defn lexicase-selection
+  "Selects an individual from the population using lexicase selection."
+  [pop argmap]
+  (loop [survivors pop
+         cases (shuffle (range (count (:errors (first pop)))))]
+    (if (or (empty? cases)
+            (empty? (rest survivors)))
+      (rand-nth survivors)
+      (let [min-err-for-case (apply min (map #(nth % (first cases))
+                                             (map :errors survivors)))]
+        (recur (filter #(= (nth (:errors %) (first cases)) min-err-for-case)
+                       survivors)
+               (rest cases))))))
+
+;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;; Uniform functions ;;;;;;;;;;;;;;;;;;;;;;
+
+(defn crossover
+  "Crosses over two individuals using uniform crossover. Pads shorter one."
+  [plushy-a plushy-b]
+  (let [shorter (min-key count plushy-a plushy-b)
+        longer (if (= shorter plushy-a)
+                 plushy-b
+                 plushy-a)
+        length-diff (- (count longer) (count shorter))
+        shorter-padded (concat shorter (repeat length-diff :crossover-padding))]
+    (remove #(= % :crossover-padding)
+            (map #(if (< (rand) 0.5) %1 %2)
+                 shorter-padded
+                 longer))))
+
+
+
+(defn uniform-addition
+  "Randomly adds new instructions before every instruction (and at the end of
+  the plushy) with some probability."
+  [plushy instructions]
+  (let [rand-code (repeatedly (inc (count plushy))
+                              (fn []
+                                (if (< (rand) 0.05)
+                                  (rand-nth instructions)
+                                  :mutation-padding)))]
+    (remove #(= % :mutation-padding)
+            (interleave (conj plushy :mutation-padding)
+                        rand-code))))
+
+(defn uniform-deletion
+  "Randomly deletes instructions from plushy at some rate."
+  [plushy]
+  (remove (fn [x] (< (rand) 0.05))
+          plushy))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
